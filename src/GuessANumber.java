8437fecf014e6d0b0f9dbaefa650f9be0e7a461a
isaacporron <isaacporron@gmail.com>
2015-04-14 21:07:26 -0700

    first commit

---
+++ src/GuessANumber.java
@@ -0,0 +1,14 @@
+
+public class GuessANumber {
+
+	public static void main(String[] args) {
+		
+		
+		
+		RandomNumber n1;
+		n1 = new RandomNumber();
+		System.out.println (n1.thenumber() );
+		
+	}
+	
+}
---
+++ src/RandomNumber.java
@@ -0,0 +1,14 @@
+
+public class RandomNumber {
+	
+	
+private int randomnumber;
+	
+	public int thenumber() {
+		
+		return randomnumber = 1 + (int) ( Math.random() * 10 );
+		
+	}
+	
+
+}
